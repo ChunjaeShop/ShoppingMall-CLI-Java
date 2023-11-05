40bf0a5d63f54aa5cca7234b790ed6c5f5fdfc1b
Jiwon <kimg1623@naver.com>
2023-11-05 18:43:19 +0900

    Feat(admin): 관리자 메인 메뉴 추가

--- src/admin/AdminMain.java
+++ src/admin/AdminMain.java
@@ -11,10 +11,41 @@ public class AdminMain {
     private Connection conn;
 
     public AdminMain() {
+
     }
 
     public void mainMenu(Connection conn) { // 3. 관리자메뉴(관리자 로그인 성공화면)
+        this.conn = conn;
+        System.out.println();
+        String menuNo;
 
+        do {
+            System.out.println("-----------------------------------------------------------------------------------");
+            System.out.println("* 관리자님 환영합니다! **");
+            System.out.println("[1.상품전체보기]  [2.주문내역조회] ");
+            System.out.println("-----------------------------------------------------------------------------------");
+            System.out.print("메뉴 선택 :");
+            menuNo = scanner.nextLine();
+
+            switch (menuNo) {
+                case "1":
+                    AllItemList(); // 3-1
+                    //listSubmenu();
+                    break;
+                case "2":
+                    //DetailItemSearch();
+                    break;
+                case "4":
+                    //itemRank();
+                    break;
+                case "9":
+                    //MyInfo();
+                    break;
+                default:
+                    System.out.println("유효하지 않은 메뉴입니다.");
+
+            }
+        }while(!menuNo.equals("1")  && !menuNo.equals("2"));
     }
 
 
