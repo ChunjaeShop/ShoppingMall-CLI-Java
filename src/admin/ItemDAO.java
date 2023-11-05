277f54a6aaa06159a78cc76068bd4ea56e5a9686
Jiwon <kimg1623@naver.com>
2023-11-05 20:10:28 +0900

    Feat(admin): ItemDAO insertItem() 메소드 추가

--- src/admin/ItemDAO.java
+++ src/admin/ItemDAO.java
@@ -23,5 +23,33 @@ public class ItemDAO {
     public void setConnection(Connection con) {
         this.conn = con;
     }
-    
+    public boolean insertItem(Item item) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            String sql =
+                    "INSERT INTO item (category_id, item_name, size, purchase_cnt, remain, price, item_contents) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, item.getCategoryId());
+            pstmt.setString(2, item.getItemName());
+            pstmt.setString(3, item.getSize());
+            pstmt.setInt(4, 0);
+            pstmt.setInt(5, item.getRemain());
+            pstmt.setInt(6, item.getPrice());
+            pstmt.setString(7, item.getContent());
+            pstmt.executeUpdate();
+            pstmt.close();
+            System.out.println("----------------상품등록 완료------------------");
+
+            return true;
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
