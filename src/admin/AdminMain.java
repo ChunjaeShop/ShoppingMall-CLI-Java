28f197d0e82d24644c346c8b1016864221ea0ae8
Jiwon <kimg1623@naver.com>
2023-11-05 18:28:46 +0900

    Feat: 관리자 로그인 후 기능 추가 AdminMain.java

---
+++ src/admin/AdminMain.java
@@ -0,0 +1,25 @@
+package admin;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AdminMain {
+    Scanner scanner = new Scanner(System.in);
+    private Connection conn;
+
+    public AdminMain() {
+    }
+
+    public void mainMenu(Connection conn) { // 3. 관리자메뉴(관리자 로그인 성공화면)
+
+    }
+
+
+    public void AllItemList() {
+    }
+
+
+}
