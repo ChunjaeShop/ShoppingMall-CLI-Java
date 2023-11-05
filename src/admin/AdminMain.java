81b0169ee0342e2d7f139c21f86edcb75464e99c
Jiwon <kimg1623@naver.com>
2023-11-05 18:56:24 +0900

    Feat(admin): 상품전체보기(3-1) 기능 추가

--- src/admin/AdminMain.java
+++ src/admin/AdminMain.java
@@ -30,7 +30,7 @@ public class AdminMain {
             switch (menuNo) {
                 case "1":
                     AllItemList(); // 3-1
-                    //listSubmenu();
+                    listSubmenu(); // 3-1 submenu
                     break;
                 case "2":
                     //DetailItemSearch();
@@ -49,8 +49,65 @@ public class AdminMain {
     }
 
 
-    public void AllItemList() {
+    public void AllItemList() { // 3-1. 상품전체보기 화면
+        // 타이틀 및 컬럼명 출력
+        System.out.println();
+        System.out.println("［상품 전체 보기］");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n", "category_id", "item_id", "item_name", "size", "purchase_cnt", "remain", "price", "item_contents");
+        System.out.println("-------------------------------------------------------------------------");
+
+        // boards 테이블에서 게시물 정보를 가져와서 출력하기
+        try {
+            String sql =
+                    "SELECT category_id, item_id, item_name, size, purchase_cnt, remain, price, item_contents " +
+                            "FROM  item ";
+            // SELECT bno, btitle, bcontent, bwriter, bdate FROM boards ORDER BY bno DESC
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                ItemDTO itemdao = new ItemDTO();
+                itemdao.setCategoryId(rs.getString("category_id"));
+                itemdao.setItemId(rs.getInt("item_id"));
+                itemdao.setItemName(rs.getString("item_name"));
+                itemdao.setSize(rs.getString("size"));
+                itemdao.setPurchaseCnt(rs.getInt("purchase_cnt"));
+                itemdao.setRemain(rs.getInt("remain"));
+                itemdao.setPrice(rs.getInt("price"));
+                itemdao.setContent(rs.getString("item_contents"));
+                System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n",
+                        itemdao.getCategoryId(),
+                        itemdao.getItemId(),
+                        itemdao.getItemName(),
+                        itemdao.getSize(),
+                        itemdao.getPurchaseCnt(),
+                        itemdao.getRemain(),
+                        itemdao.getPrice(),
+                        itemdao.getContent());
+            }
+            rs.close();
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void listSubmenu() { // 3-1 submenu
+        String menuNo;
+        do {
+            System.out.println("-----------------------------------------------------------------------------------");
+            System.out.println("[1.상품등록]   [2.상품정보수정]   [3.상품삭제]   [9.뒤로가기]");
+            System.out.println("-----------------------------------------------------------------------------------");
+            System.out.println("메뉴 선택 :");
+            menuNo = scanner.nextLine();
+
+            switch (menuNo) {
+                case "1":
+            }
+        } while (!menuNo.equals("1") && !menuNo.equals("2") && !menuNo.equals("3"));
+    }
+
+
 
 
 }
