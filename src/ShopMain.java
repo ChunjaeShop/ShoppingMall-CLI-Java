9ef4f12310bf9c6fa2ef2e86eda2f388c207b486
jiho <yooy1313@naver.com>
2023-11-07 17:32:17 +0900

    Fix(merger) : viewmenu 출력 코드 삭제

--- src/ShopMain.java
+++ src/ShopMain.java
@@ -12,7 +12,7 @@ public class ShopMain { // main 메소드 실행하는 클래스
         menuView = new MenuView(dbUtil.init());
         menuView.printDisabledMainMenu(); // 프로그램 시작했을 때 비활성화 메인 메뉴 출력
         int viewMenu = menuView.printLoginMenu(); // 이어서 로그인/회원가입 메뉴 보이기 -> 입력받기
-        System.out.println("viewMenu : " + viewMenu);
+
         switch (viewMenu){
             case 1: // user logged in
                 userView = new UserView(dbUtil.init());
